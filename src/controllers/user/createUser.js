33ed074c58fc945f1336c4de23d0e600f3448f1a
alegandolfo <alegandolfo10@gmail.com>
2023-10-10 20:35:11 -0300

    chore: add data validation on create user

--- src/controllers/user/createUser.js
+++ src/controllers/user/createUser.js
@@ -1,20 +1,30 @@
+import zodErrorFormat from "../../helpers/zodErrorFormat.js"
 import userModel from "../../models/userModel.js"
 
 const createUser = async (req, res) => {
     try {
-        const [result] = await userModel.create(req.body)
-        if (result.affectedRows === 1) {
-            const newUser = req.body
-            delete newUser.password
+      const userValidated = userModel.validateCreateUser(req.body)
+      if(userValidated.success === false) {
+        const zodError = zodErrorFormat(userValidated.error)
+        return res.status(400).json({
+          error: 'Invalid data',
+          fields: zodError
+        })
+      }
 
-            res.status(201).json({
-              success: 'Usuário criado com sucesso',
-              user: {
-                id: result.insertId,
-                ...req.body
-              }
-            })
-        }
+      const [result] = await userModel.create(req.body)
+      if (result.affectedRows === 1) {
+        const newUser = req.body
+        delete newUser.password
+
+        res.status(201).json({
+          success: 'Usuário criado com sucesso',
+          user: {
+            id: result.insertId,
+            ...req.body
+          }
+        })
+      }
         
     } catch (err) {
         console.error(err)
--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,4 +1,49 @@
 import db from "../database/db.js"
+import { z } from 'zod'
+
+const userSchema = z.object({
+  id: 
+    z.number({
+      required_error: 'ID is required.',
+      invalid_type_error: 'ID must be a number.'
+    }),
+  name: 
+    z.string({
+      required_error: 'NAME is required.',
+      invalid_type_error: 'NAME must be a string.'
+    })
+    .min(3, {message: 'NAME must have at least 3 characters.'})
+    .max(200, {message: 'NAME must have no more than 200 characters'}),
+  email: 
+      z.string({
+      required_error: 'EMAIL is required.',
+      invalid_type_error: 'EMAIL must be a string.'
+    })
+    .email({message: 'EMAIL is not valid.'})
+    .min(5, {message: 'EMAIL must have at least 5 characters.'})
+    .max(500, {message: 'EMAIL must have no more than 500 characters'}),
+  password: 
+    z.string({
+      required_error: 'PASSWORD is required.',
+      invalid_type_error: 'PASSWORD must be a string.'
+    }),
+    // .regex(new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,10}$"), {message: 'PASSWORD is not valid.'}),
+  image:
+    z.string({
+      required_error: 'IMAGE is required.',
+      invalid_type_error: 'IMAGE must be a string.'
+    })
+    .url({message: 'IMAGE must be a url.'})
+})
+
+const validateCreateUser = (user) => {
+  const partialUserSchema = userSchema.partial({id: true})
+  return partialUserSchema.safeParse(user)
+}
+
+const validateUpdateUser = (user) => {
+  return userSchema.safeParse(user)
+}
 
 const list = async () => {
     return await db.query('SELECT * FROM users')
@@ -25,4 +70,4 @@ const remove = async (url) => {
     return await db.query('DELETE FROM users WHERE id = ?', [id])
 }
 
-export default {list, create, get, update, remove}
+export default {list, create, get, update, remove, validateCreateUser}
